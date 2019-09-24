0df6317982aefe94857ebf57c86f9962804a28d6
James Ward <james@jamesward.com>
2019-09-24 07:15:10 -0600

    add support for postdeploy script - fixes #76

--- cmd/cloudshell_open/appfile.go
+++ cmd/cloudshell_open/appfile.go
@@ -32,9 +32,14 @@ type env struct {
 	Required    *bool  `json:"required"`
 }
 
+type scripts struct {
+	Postdeploy string `json:"postdeploy"`
+}
+
 type appFile struct {
-	Name string         `json:"name"`
-	Env  map[string]env `json:"env"`
+	Name    string         `json:"name"`
+	Env     map[string]env `json:"env"`
+	Scripts scripts        `json:"scripts"`
 
 	// The following are unused variables that are still silently accepted
 	// for compatibility with Heroku app.json files.
